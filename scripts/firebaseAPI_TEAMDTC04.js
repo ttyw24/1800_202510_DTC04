badb204f0a89be96b80baff4a8aae170ffee6fbb
Shokisnipes <plazic2@my.bcit.ca>
2025-03-11 16:06:17 -0700

    Rewrite code to connect firebase to project

---
+++ scripts/firebaseAPI_TEAMDTC04.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGPCCfSJIZDIaqXFKv9vYktpeBdWcQVUk",
+  authDomain: "comp1800-202510-dtc04.firebaseapp.com",
+  projectId: "comp1800-202510-dtc04",
+  storageBucket: "comp1800-202510-dtc04.firebasestorage.app",
+  messagingSenderId: "928228316192",
+  appId: "1:928228316192:web:5641ce47000d06022e5845"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
